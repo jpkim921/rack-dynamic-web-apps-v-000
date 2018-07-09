0ded8f3ddfdeb2942371a654261358683c1086ae
Jayon Kim <jpkim921@gmail.com>
2018-07-09 01:34:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,14 @@ class Application
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
 
+    num_1 = Kernal.rand(1..20)
+    num_2 = Kernal.rand(1..20)
+    num_3 = Kernal.rand(1..20)
+
+    resp.write "#{num_1}\n"
+    resp.write "#{num_1}\n"
+    resp.write "#{num_1}\n"
+
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
     else
